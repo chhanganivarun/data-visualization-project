d4ce6fd32afedefd55a0d435e969f5f62358e7af
Varun Chhangani <varunchhangani@gmail.com>
2020-05-02 17:10:48 +0530

    add dataframes

--- phase3/index.py
+++ phase3/index.py
@@ -8,7 +8,62 @@ external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
     './assets/index.css',
 ]
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+country_series = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDScountry-series.csv')
+country_series.drop(['Unnamed: 3'], axis=1, inplace=True)
+
+country = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDSCountry.csv')
+country.drop(['Unnamed: 31'], axis=1, inplace=True)
+
+idsdata = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDSData.csv')
+idsdata.drop(['Unnamed: 61'], axis=1, inplace=True)
+
+footnote = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDSfootnote.csv')
+footnote.drop(['Unnamed: 4'], axis=1, inplace=True)
+
+series = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDSSeries.csv')
+series.drop(['Unnamed: 20'], axis=1, inplace=True)
+
+series_time = pd.read_csv(
+    'https://raw.githubusercontent.com/chhanganivarun/data-visualization-project/master/IDS_CSV/IDSseries-time.csv')
+series_time.drop(['Unnamed: 3'], axis=1, inplace=True)
+
+ind_str = """
+Net financial flows, others
+Net financial flows, bilateral (NFL, current US$)"""
+indicators = [x.strip() for x in ind_str.split('\n') if x.strip() != '']
+indicators
+codes = list()
+for x in indicators:
+    codes += series[series['Indicator Name']
+                    .str.find(x) > -1]['Series Code'].to_list()
+codes
+
+countries = idsdata[['Country Code', 'Country Name']]
+countries = countries.drop_duplicates()
+countries_codes_names = [(x, y) for x, y in zip(
+    countries['Country Code'].to_list(), countries['Country Name'].to_list())]
+
+all_indicators = pd.unique(idsdata['Indicator Name'])
+
+country_codes_str = """
+AFG
+IND
+PAK
+CHN"""
+country_codes = [x.strip()
+                 for x in country_codes_str.split('\n') if x.strip() != '']
+
+indicator_codes = codes
+
+idsdata[(idsdata['Country Code'].isin(country_codes)) &
+        (idsdata['Indicator Code'].isin(indicator_codes))]
 
 
 def generate_table(dataframe, max_rows=10):
@@ -28,7 +83,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 side_elements = html.Div(className='column', children=[
     html.Div(className='table-pane', children=[
         html.Label('Data'),
-        generate_table(df, np.inf),
+        generate_table(idsdata, np.inf),
     ]),
     html.Div(className='text-pane', children=[
         html.Label('text'),
@@ -40,48 +95,16 @@ side_elements = html.Div(className='column', children=[
 main_space = html.Div(className="mid-pane", children=[
     html.Label('Dropdown'),
     dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='MTL'
+        options=[{'label': x, 'value': y} for x, y in countries_codes_names],
+        value='IND'
     ),
 
     html.Label('Multi-Select Dropdown'),
     dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF'],
+        options=[{'label': x, 'value': y} for x, y in countries_codes_names],
+        value=['IND', 'CHN'],
         multi=True
     ),
-
-    html.Label('Radio Items'),
-    dcc.RadioItems(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='MTL'
-    ),
-
-    html.Label('Checkboxes'),
-    dcc.Checklist(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF']
-    ),
-
-    html.Label('Text Input'),
-    dcc.Input(value='MTL', type='text'),
-
     html.Label('Slider'),
     dcc.Slider(
         min=0,
